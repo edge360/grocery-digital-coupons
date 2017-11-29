e50cb4386d6561c1f891868bd2cebee0325ac6c0
Kory Becker <kbecker@primaryobjects.com>
2017-11-29 04:08:34 +0000

    Cleanup.

--- web.py
+++ web.py
@@ -48,7 +48,6 @@ def post_collect():
             # Run the method asynchronously.
             pool = Pool(processes=3)
             pool.apply_async(grocery_coupons.shoprite, args=(username, password, key, 10), callback=onComplete)
-            #onComplete(grocery_coupons.shoprite(username, password, key, 10))
 
     # Return an html or json view depending on the client.
     return redirect('/result/' + key if 'text/html' in request.headers.get('Accept') else '/collect/' + key) if key in data else jsonify({ 'status': 'MISSING LOGIN' })
