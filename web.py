8f1e389512c2ee61a71f6bbcf43344f196cf0150
Kory Becker <kbecker@primaryobjects.com>
2017-12-05 15:12:45 -0500

    Added support for token in request.headers('token'). Removed pass from being stored in the token. Added error checking.

--- web.py
+++ web.py
@@ -16,17 +16,19 @@ from ConfigParser import RawConfigParser
 app = flask.Flask(__name__)
 sslify = SSLify(app)
 
-data = {}
-secret = 'x0Wm5hfk78cBaG2MkM1d'
+data = {} # In-memory session.
+auth = {} # In-memory storage of user auth data (user/pass).
+
+secret = 'x0Wm5hfk78cBaG2MkM1d' # Token secret key.
 
 @app.before_request
 def before_request():
+    g.error = None
+
     # Validate a token before each api request.
     if '/api' in request.path:
-        token = request.args.get('token', type = str)
+        token = request.args.get('token', type = str) or request.headers.get('token', type = str)
         if token:
-            g.error = None
-
             try:
                 # Set the request context to include the payload from the token.
                 g.payload = jwt.decode(token, secret, algorithms=['HS256'])
@@ -41,36 +43,52 @@ def before_request():
 def login():
     token = None
 
-    data = request.json
-    if data:
-        username = data['username'] if 'username' in data else None
-        password = data['password'] if 'password' in data else None
+    content = request.json
+    if content:
+        username = content['username'] if 'username' in content else None
+        password = content['password'] if 'password' in content else None
 
         if username and password:
             # Generate a token.
-            token = jwt.encode({ 'username': username, 'password': password, 'exp': datetime.utcnow() + timedelta(minutes = 10) }, secret, algorithm = 'HS256')
+            token = jwt.encode({ 'username': username, 'exp': datetime.utcnow() + timedelta(minutes = 10) }, secret, algorithm = 'HS256')
+
+            # Set user in session variable.
+            data[username] = {
+                'username': username,
+                'startDate': datetime.now(),
+                'status': 'IDLE',
+                'count': 0,
+                'existingCount': 0
+            }
+
+            # Set pass in session variable.
+            auth[username] = password;
 
             # Collect coupons.
             onCollect(username, password)
 
-    return jsonify({ 'token': token }) if token else jsonify({ 'error': 'Invalid username or password.' })
+    return jsonify({ 'token': token }) if token else jsonify({ 'error': 'Invalid username or password.' }), 200 if token else 401
 
 @app.route('/api/coupons', methods = ['POST'])
 def coupons():
     error = None
+    message = None
+    noData = False
 
     if not g.error:
         payload = g.payload
         username = payload['username']
-        password = payload['password']
+        
+        password = auth[username] if username in auth else None
 
         # Collect coupons.
-        onCollect(username, password)
+        error = onCollect(username, password)
+        noData = True if error and 'No data' in error else False
     else:
         error = 'Invalid token.'
         message = str(g.exception)
-
-    return jsonify({ 'status': 'Running' }) if not error else jsonify({ 'error': error, 'message': message }), 200 if not error else 401
+    
+    return jsonify({ 'status': 'Running' }) if not error else jsonify({ 'error': error, 'message': message }), 200 if not error else 404 if noData else 401
 
 @app.route('/api/status')
 def status():
@@ -107,6 +125,9 @@ def delete():
         username = payload['username']
 
         # Delete the username.
+        if username in auth:
+            auth.pop(username)
+
         error = None if username in data and data.pop(username) else 'No data for ' + username
         noData = True if error else False
     else:
@@ -121,26 +142,26 @@ def loginView():
 
 @app.route('/')
 def index():
-    token = request.args.get('token', type = str)
-
-    return render_template('index.html') if token else redirect('/login')
+    return render_template('index.html') if not g.error else redirect('/login')
 
 def onCollect(username, password):
-    if not username in data:
-        # Set new user.
-        data[username] = {
-            'username': username,
-            'startDate': datetime.now(),
-            'status': 'IDLE',
-            'count': 0,
-            'existingCount': 0
-        }
+    error = None
 
-    if data[username]['status'] != 'RUNNING':
-        # Run the method asynchronously.
-        data[username]['status'] = 'RUNNING'
-        thread = Thread(target=grocery_coupons.shoprite, args=(username, password, 10, onStatus))
-        thread.start()
+    if username and password:
+        if username in data:
+            if data[username]['status'] != 'RUNNING':
+                # Launch the Selenium coupon collection process asynchronously.
+                data[username]['status'] = 'RUNNING'
+                thread = Thread(target=grocery_coupons.shoprite, args=(username, password, 10, onStatus))
+                thread.start()
+            else:
+                error = 'Already running.'
+        else:
+            error = 'No data for ' + username
+    else:
+        error = 'Invalid username or password.'
+
+    return error
 
 def onStatus(status):
     global data
