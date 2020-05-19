eb94b974ed0fbad72765e635c37e3c801cfd14ea
Kory Becker <kbecker@primaryobjects.com>
2020-05-19 15:31:46 -0400

    Version.

--- web.py
+++ web.py
@@ -20,7 +20,7 @@ sslify = SSLify(app)
 data = {} # In-memory session.
 
 secret = 'x0Wm5hfk78cBaG2MkM1d' # Token secret key.
-version = 2.9
+version = 2.91
 
 @app.before_request
 def before_request():
