370133d61185dc9a0b75c94ed1d9b55d48b25e3f
Kory Becker <kbecker@primaryobjects.com>
2020-07-22 18:30:41 -0400

    Added command-line args.

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ subprocess.call(['python3', 'setup.py'])
 import sys
 import os
 import grocery_coupons
+import argparse
 from configparser import RawConfigParser
 
 def onStatus(status):
@@ -13,20 +14,26 @@ def onStatus(status):
         print(status['error'])
 
 if __name__ == "__main__":
+    arparser = argparse.ArgumentParser(description='Grocery Digital Coupons.')
+    arparser.add_argument('--config', type=str, default='shoprite', help='Config section to read login from.')
+    arparser.add_argument('--store', type=str, default='shoprite', nargs='?', help='Store to clip coupons [shoprite, stop_and_shop].')
+    arparser.add_argument('--user', type=str, nargs='?', help='Login username or read from config.ini.')
+    arparser.add_argument('--password', type=str, nargs='?', help='Login password or read from config.ini.')
+    args = arparser.parse_args()
+
     parser = RawConfigParser()
     parser.read('config.ini')
 
     # Get email/password from config file
-    email = os.getenv('email') or parser.get('shoprite', 'email')
-    password = os.getenv('password') or parser.get('shoprite', 'password')
+    email = os.getenv('email') or args.user or parser.get(args.config, 'email')
+    password = os.getenv('password') or args.password or parser.get(args.config, 'password')
     delay = 10
 
-    if len(sys.argv) > 1:
-        if sys.argv[1] == 'shoprite':
-            grocery_coupons.shoprite(email, password, delay, onStatus)
-        elif sys.argv[1] == 'stop_and_shop':
-            grocery_coupons.stop_and_shop(email, password, delay, onStatus)
-        else:
-            print('Unknown site [' + sys.argv[1] + ']')
-    else:
+    if args.store == 'shoprite':
+        grocery_coupons.shoprite(email, password, delay, onStatus)
+    elif args.store == 'stop_and_shop':
+        grocery_coupons.stop_and_shop(email, password, delay, onStatus)
+    elif args.store == 'help':
         print('Usage: client.py [shoprite | stop_and_shop]')
+    else:
+        print('Unknown store: ' + args.store)
