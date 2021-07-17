308988ce9a77515ccc344e33bf7e2e132584e468
Kory Becker <kbecker@primaryobjects.com>
2021-07-17 15:23:52 -0400

    Download specific version of Chrome to match driver.

--- chromedriver.py
+++ chromedriver.py
@@ -15,6 +15,13 @@ def get_driver():
     # Attempt to open the Selenium chromedriver. If it fails, download the latest chromedriver.
     driver = None
     retry = True
+    major_version = None
+
+    # Determine the version of Chrome installed.
+    version = get_chrome_version()
+    if version:
+        parts = version.split('.')
+        major_version = parts[0] if len(parts) > 0 else 0
 
     while retry:
         retry = False
@@ -36,18 +43,18 @@ def get_driver():
             elif "chromedriver' executable needs to be in PATH" in e.msg:
                 is_download = True
 
-        retry = is_download and download_driver()
+        retry = is_download and download_driver(major_version)
 
     return driver
 
-def download_driver():
+def download_driver(version=None):
     # Find the latest chromedriver, download, unzip, set permissions to executable.
     result = False
     url = 'https://chromedriver.chromium.org/downloads'
     base_driver_url = 'https://chromedriver.storage.googleapis.com/'
     file_name = 'chromedriver_' + get_platform_filename()
     driver_file_name = 'chromedriver' + '.exe' if platform == "win32" else ''
-    pattern = 'https://.*?path=(\d+\.\d+\.\d+\.\d+)'
+    pattern = 'https://.*?path=(' + (version or '\d+') + '\.\d+\.\d+\.\d+)'
 
     # Download latest chromedriver.
     print('Finding latest chromedriver..')
@@ -107,3 +114,38 @@ def get_platform_filename():
     filename += '.zip'
 
     return filename
+
+def extract_version(output):
+    try:
+        google_version = ''
+        for letter in output[output.rindex('DisplayVersion    REG_SZ') + 24:]:
+            if letter != '\n':
+                google_version += letter
+            else:
+                break
+        return(google_version.strip())
+    except TypeError:
+        return
+
+def get_chrome_version():
+    version = None
+    install_path = None
+
+    try:
+        if platform == "linux" or platform == "linux2":
+            # linux
+            install_path = "/usr/bin/google-chrome"
+        elif platform == "darwin":
+            # OS X
+            install_path = "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
+        elif platform == "win32":
+            # Windows...
+            stream = os.popen('reg query "HKLM\\SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Google Chrome"')
+            output = stream.read()
+            version = extract_version(output)
+    except Exception as ex:
+        print(ex)
+
+    version = os.popen(f"{install_path} --version").read().strip('Google Chrome ').strip() if install_path else version
+
+    return version
