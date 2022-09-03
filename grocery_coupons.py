287f88d19fcf94d1d59a05e1a52ed35460872a7c
edge360 <edge360@gmail.com>
2022-09-03 18:21:55 -0400

    Update grocery_coupons.py
    completely reworked code. shoprite only until I can check other sites. Arg parsing needs some looking at.

--- grocery_coupons.py
+++ grocery_coupons.py
@@ -1,386 +1,129 @@
 import os
 import time
-from pytextbelt import Textbelt
+import argparse
+from configparser import RawConfigParser
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import UnexpectedAlertPresentException
-from pyvirtualdisplay import Display
+from pyvirtualdisplay.display import Display
 
-display = Display(visible=0, size=(800, 600))
-display.start()
-browser = None
+
+
+driver = None
+display = None
+
+def shoprite(email, password):
+
+    #initialize browser and virt display
+    initialize()
+
+    try:
+        print("Loading Page...")
+        driver.get('https://www.shoprite.com/digital-coupon')
+
+        print("Navigating to Login Page...")
+        wait = WebDriverWait(driver, 60)
+        wait.until(EC.element_to_be_clickable((By.ID, "AccountHeaderButton"))).click()
+
+
+        print("Logging In...")
+        wait.until(EC.element_to_be_clickable((By.ID, "Email"))).send_keys(email)
+        driver.find_element(By.ID,'Password').send_keys(password)
+        driver.find_element(By.ID,'Password').send_keys(Keys.RETURN)
+        print("Logged In...")
+        time.sleep(1)
+
+        print("Redirecting back to Coupon page...")
+        wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, "sr-digital-coupons")))
+
+        print("Loading All Coupons to page...")
+        wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "btn.btn-default.btn-sm.ng-tns-c98-0.ng-star-inserted"))).click()
+
+        print("Finding  Coupons...")
+        coupon_count = 0
+        coupons = driver.find_elements(By.CLASS_NAME, 'coupon-item-container')
+        for coupon in coupons:
+            coupon_name = coupon.find_element(By.CLASS_NAME, 'coupon-brand-name').text
+            coupon_desc = coupon.find_element(By.CLASS_NAME, 'coupon-desc').text
+            try:
+                coupon.find_element(By.CLASS_NAME, 'available-to-clip.ng-star-inserted').click()
+                print("Clipped: " + str(coupon_name) + " --- " + str(coupon_desc))
+                count += 1
+            except:
+                #print("FAIL - Already Clipped")
+                pass
+
+        if coupon_count:
+            print("Clipped " + coupon_count + "coupons :)")
+        else:
+            print("No Coupons Available")
+
+        #close/quit chrome and stop display
+        driver.close()
+        driver.quit()
+        display.stop()
+
+
+    except Exception as e:
+        #close/quit chrome and stop display on exception
+        print(e)
+        driver.close()
+        driver.quit()
+        display.stop()
 
 def initialize():
-    global browser
+    global driver
+    global display
 
-    path = os.getenv('GOOGLE_CHROME_SHIM') or None
+    #ran with virtual display to avoid captchas when using headless
+    display = Display(visible=0, size=(1200, 1000))
+    display.start()
 
-    options = webdriver.ChromeOptions()
-    options.binary_location = path
-    #options.add_experimental_option('w3c', False)
-    #if path:
-    #    options.add_argument('headless')
-    options.add_argument("--remote-debugging-port=9222")
-    options.add_argument('--no-sandbox')
+    options = Options()
+    options.add_argument("--enable-javascript")
     options.add_argument('--disable-dev-shm-usage')
-    
-    executable_path = 'chromedriver' if 'DYNO' in os.environ else './chromedriver'
-    browser = webdriver.Chrome(executable_path=executable_path, options = options)
+    options.add_argument('window-size=1200x1000')
+    options.add_argument('start-maximized')
 
-    print('Using ' + (path or executable_path))
+    driver = webdriver.Chrome(options=options)
 
-def test(email, password, delay = 10, callback = None):
-    result = { 'email': email, 'existingCount': 0, 'count': 0, 'message': None, 'screenshot': None }
+    print('Browser initialized')
 
-    if callback:
-        callback(result)
 
-    return result
+def stop_and_shop(email, password):
+    pass
 
-def shoprite(email, password, phone = None, delay = 10, callback = None):
-    result = { 'email': email, 'existingCount': 0, 'count': 0, 'message': None, 'screenshot': None }
+def acme(email,password):
+    pass
 
-    initialize()
 
-    if callback:
-        result['message'] = 'Navigating to home page.'
-        callback(result)
 
-    try:
-        browser.get('https://coupons.shoprite.com')
+if __name__ == '__main__':
 
-        if callback:
-            result['message'] = 'Locating sign-in page.'
-            callback(result)
+    arparser = argparse.ArgumentParser(description='Grocery Digital Coupons.')
+    arparser.add_argument('--config', type=str, default='shoprite', help='Config section to read login from.')
+    arparser.add_argument('--store', type=str, default='shoprite', nargs='?', help='Store to clip coupons [shoprite, acme, stop_and_shop].')
+    arparser.add_argument('--user', type=str, nargs='?', help='Login username or read from config.ini.')
+    arparser.add_argument('--password', type=str, nargs='?', help='Login password or read from config.ini.')
+    args = arparser.parse_args()
 
-        for i in range(3):
-            try:
-                # Wait for page load.
-                WebDriverWait(browser, delay).until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, 'a.login-to-load'))
-                )
-                break
-            except Exception as e:
-                result['message'] = 'Unable to load sign-in page, reloading page. Attempt ' + str(i+1) + '/3.'
-                result['screenshot'] = browser.get_screenshot_as_base64()
-                callback(result)
-                browser.refresh()
-
-        browser.find_elements_by_css_selector('a.login-to-load')[0].click()
-
-        if callback:
-            result['message'] = 'Waiting for login page.'
-            result['screenshot'] = browser.get_screenshot_as_base64()
-            callback(result)
-
-        # Login
-        time.sleep(2)
-        WebDriverWait(browser, 60).until(
-            EC.presence_of_element_located((By.ID, "Email"))
-        )
-
-        if callback:
-            result['message'] = 'Entering login details.'
-            callback(result)
-
-        # Send login info.
-        browser.find_element_by_id('Email').send_keys(email)
-        browser.find_element_by_id('Password').send_keys(password)
-        browser.find_element_by_id('Password').send_keys(Keys.RETURN)
-
-        if callback:
-            result['message'] = 'Signing in.'
-            result['screenshot'] = browser.get_screenshot_as_base64()
-            callback(result)
-
-        # Wait until the site loads, find the welcome page or error message.
-        WebDriverWait(browser, delay).until(
-            EC.visibility_of_element_located((By.CSS_SELECTOR, '.coupon-savings-count, .field-validation-error, .validation-summary-errors'))
-        )
-
-        if callback:
-            result['message'] = 'Checking if login succeeded.'
-            callback(result)
-
-        # Check if the login succeeded.
-        fields = browser.find_elements_by_xpath("//*[contains(text(), 'incorrect') or contains(text(), 'try again')]")
-        if len(fields) > 0:
-            # Invalid login?
-            result['message'] = 'Error'
-            result['error'] = 'Invalid login.'
-            result['screenshot'] = browser.get_screenshot_as_base64()
-            callback(result)
-            count = -1
-        else:
-            if callback:
-                result['message'] = 'Navigating to coupons.'
-                callback(result)
-
-            WebDriverWait(browser, delay).until(
-                EC.visibility_of_element_located((By.CSS_SELECTOR, '.coupon-item-container'))
-            )
-
-            if callback:
-                result['message'] = 'Loading coupons.'
-                result['screenshot'] = browser.get_screenshot_as_base64()
-                callback(result)
-
-            WebDriverWait(browser, delay).until(
-                EC.visibility_of_element_located((By.CLASS_NAME, "coupon-item"))
-            )
-
-            # Read all coupons on the current page, then process all subsequent pages by clicking Next, until no more pages.
-            if callback:
-                result['message'] = 'Reading coupons.'
-                callback(result)
-
-            try:
-                btnShowAll = browser.find_elements_by_xpath("//div[contains(@class, 'coupon-app')]/descendant::button[contains(text(), 'Show All')]")
-                if len(btnShowAll) > 0:
-                    btnShowAll[0].click()
-
-                result['existingCount'] = len(browser.find_elements_by_class_name('clipped-coupon-circle'))
-                result['screenshot'] = browser.get_screenshot_as_base64()
-
-                # Click all the buttons to add the coupons to your card
-                list_of_coupon_buttons = browser.find_elements_by_css_selector("a.available-to-clip:not(.ng-hide)")
-
-                for count, coupon_button in enumerate(list_of_coupon_buttons, start=1):
-                    # Check for a modal notice dialog.
-                    modals = browser.find_elements_by_class_name('modal-dialog')
-                    if modals:
-                        modal = modals[0]
-
-                        # Find the dialog title.
-                        titles = modal.find_elements_by_class_name('modal-title')
-                        titleText = titles[0].text if titles else ''
-
-                        bodies = modal.find_elements_by_class_name('modal-body')
-                        bodyText = bodies[0].text if bodies else ''
-
-                        if callback:
-                            result['message'] = 'Found dialog "' + titleText + '": ' + bodyText
-                            callback(result)
-
-                            # Try to find the 'OK' button, otherwise click the first button found.
-                            buttons = modal.find_elements_by_xpath("button[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), 'ok')]")
-                            if not buttons:
-                                buttons = modal.find_elements_by_xpath("//button[contains(text(), 'Close')]")
-                                if not buttons:
-                                    # Just find the first button.
-                                    buttons = modal.find_elements_by_class_name('btn')
-
-                            if buttons:
-                                # Click the button to accept the dialog.
-                                buttons[0].click()
-
-                    # Click the "Load to Card" button.
-                    coupon_button.click()
-
-                    if callback:
-                        result['count'] += 1
-                        result['message'] = 'Added ' + str(result['count']) + '. Already clipped ' + str(result['existingCount']) + '.'
-                        if count % 10 == 0:
-                            result['screenshot'] = browser.get_screenshot_as_base64()
-                        callback(result)
-
-                    time.sleep(.5)
-
-                if callback:
-                    result['screenshot'] = browser.get_screenshot_as_base64()
-                    callback(result)
-            except UnexpectedAlertPresentException as e:
-                print("Dismissing alert " + repr(e))
-                alert = browser.switch_to_alert()
-                alert.accept()
-            except Exception as e:
-                print(e)
-
-            if callback:
-                result['message'] = 'Complete!'
-                if phone and result['count'] > 0:
-                    summary = 'Couponfire clipped ' + str(result['count']) + ' coupons. You now have ' + str(result['count'] + result['existingCount']) + ' total.'
-                    recipient = Textbelt.Recipient(phone)
-                    response = recipient.send(summary)
-                    print(summary)
-                    print(response)
-                callback(result)
-    except UnexpectedAlertPresentException as e:
-        alert = browser.switch_to_alert()
-        alert.accept()
-    except Exception as e:
-        if callback:
-            print(e)
-            result['message'] = 'Error'
-            result['error'] = repr(e)
-            result['screenshot'] = browser.get_screenshot_as_base64()
-            callback(result)
-
-    browser.close()
-
-    return result
-
-def stop_and_shop(email, password, phone = None, delay = 10, callback = None, complete = None):
-    initialize()
+    parser = RawConfigParser()
+    parser.read('config.ini')
 
     # Get email/password from config file
-    email = parser.get('stop and shop', 'email')
-    password = parser.get('stop and shop', 'password')
+    email = os.getenv('email') or args.user or parser.get(args.config, 'email')
+    password = os.getenv('password') or args.password or parser.get(args.config, 'password')
 
-    # Visit the Login page
-    browser.get('https://stopandshop.com/login/')
-
-    # Login
-    help_element = browser.find_element_by_link_text('help')
-    help_element.send_keys(Keys.TAB)
-    username_element = browser.switch_to.active_element
-    username_element.send_keys(email)
-    username_element.send_keys(Keys.TAB)
-    password_element = browser.switch_to.active_element
-    password_element.send_keys(password)
-    password_element.send_keys(Keys.RETURN)
-
-    # Wait until link to go to coupons page is ready
-    WebDriverWait(browser, delay).until(
-        EC.presence_of_element_located((By.LINK_TEXT, 'coupons'))
-    )
-
-    # Go to coupons page
-    browser.get('https://stopandshop.com/dashboard/coupons-deals/')
-
-    # Wait until coupons are ready
-    WebDriverWait(browser, delay).until(
-        EC.presence_of_element_located((By.CLASS_NAME, 'load-to-card'))
-    )
-
-    # Click all the buttons to add the coupons to your card
-    list_of_coupon_buttons = browser.find_elements_by_class_name('load-to-card')
-
-    for count, coupon_button in enumerate(list_of_coupon_buttons, start=1):
-        try:
-            coupon_button.click()
-            if callback:
-                result['message'] = 'Added ' + str(count) + ' coupons!'
-                result['count'] = count
-                callback(result)
-            time.sleep(1)
-        except:
-            continue
-
-    result['screenshot'] = browser.get_screenshot_as_base64()
-
-    if callback:
-        result['message'] = 'Complete!'
-        callback(result)
-
-    browser.close()
-
-    return result
-
-def acme(email, password, phone = None, delay = 10, callback = None):
-    result = { 'email': email, 'existingCount': 0, 'count': 0, 'message': None, 'screenshot': None }
-
-    initialize()
-
-    if callback:
-        result['message'] = 'Navigating to home page.'
-        callback(result)
-
-    try:
-        browser.get('https://www.acmemarkets.com/account/sign-in.html?goto=/content/www/acmemarkets/en/justforu/coupons-deals.html')
-
-        if callback:
-            result['message'] = 'Locating sign-in page.'
-            callback(result)
-
-            # Wait for page load.
-            WebDriverWait(browser, delay).until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, '#label-email'))
-            )
-
-        # Login
-        if callback:
-            result['message'] = 'Entering login details.'
-            callback(result)
-
-        # Send login info.
-        browser.find_element_by_id('label-email').send_keys(email)
-        browser.find_element_by_id('label-password').send_keys(password)
-        browser.find_element_by_id('label-password').send_keys(Keys.RETURN)
-
-        if callback:
-            result['message'] = 'Signing in.'
-            result['screenshot'] = browser.get_screenshot_as_base64()
-            callback(result)
-
-        # Wait until the site loads, find the welcome page or error message.
-        WebDriverWait(browser, delay).until(
-            EC.visibility_of_element_located((By.CSS_SELECTOR, 'button.load-more'))
-        )
-
-        # Read all coupons on the current page, then process all subsequent pages by clicking Next, until no more pages.
-        if callback:
-            result['message'] = 'Reading coupons.'
-            callback(result)
-
-        try:
-            btnLoadMore = browser.find_element_by_css_selector('button.load-more');
-            while btnLoadMore:
-                btnLoadMore.click();
-                time.sleep(1)
-                try:
-                    btnLoadMore = browser.find_element_by_css_selector('button.load-more');
-                except:
-                    break
-
-            result['existingCount'] = len(browser.find_elements_by_class_name('coupon-clipped-container'))
-            result['screenshot'] = browser.get_screenshot_as_base64()
-
-            # Click all the buttons to add the coupons to your card
-            list_of_coupon_buttons = browser.find_elements_by_css_selector("button.grid-coupon-btn")
-
-            for count, coupon_button in enumerate(list_of_coupon_buttons, start=1):
-                try:
-                    # Click the "Load to Card" button.
-                    coupon_button.click()
-
-                    if callback:
-                        result['count'] += 1
-                        result['message'] = 'Added ' + str(result['count']) + '. Already clipped ' + str(result['existingCount']) + '.'
-                        if count % 10 == 0:
-                            result['screenshot'] = browser.get_screenshot_as_base64()
-                        callback(result)
-
-                    time.sleep(.25)
-                except:
-                    continue
-
-            if callback:
-                result['screenshot'] = browser.get_screenshot_as_base64()
-                callback(result)
-        except Exception as e:
-            print(e)
-
-        if callback:
-            result['message'] = 'Complete!'
-            if phone and result['count'] > 0:
-                summary = 'Couponfire clipped ' + str(result['count']) + ' coupons. You now have ' + str(result['count'] + result['existingCount']) + ' total.'
-                recipient = Textbelt.Recipient(phone)
-                response = recipient.send(summary)
-                print(summary)
-                print(response)
-            callback(result)
-    except Exception as e:
-        if callback:
-            print(e)
-            result['message'] = 'Error'
-            result['error'] = repr(e)
-            result['screenshot'] = browser.get_screenshot_as_base64()
-            callback(result)
-
-    browser.close()
-
-    return result
+    if args.store == 'shoprite':
+        shoprite(email, password)
+    elif args.store == 'stop_and_shop':
+        stop_and_shop(email, password)
+    elif args.store == 'acme':
+        acme(email, password)
+    elif args.store == 'help':
+        print('Usage: coupons.py [shoprite | acme | stop_and_shop]')
+    else:
+        print('Unknown store: ' + args.store)
